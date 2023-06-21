6805a79f1ccbf1c6aa7b9860481a3c35fa40e97d
mrjvs <jellevs@gmail.com>
2023-06-21 15:12:45 +0200

    update more configs and stuff

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,26 @@ const { defineConfig } = require('vitest/config');
 const { default: eslint } = require('vite-plugin-eslint');
 const dts = require('vite-plugin-dts');
 
+const main = path.resolve(__dirname, 'src/index.ts');
+
 module.exports = defineConfig({
-  plugins: [eslint(), dts()],
+  plugins: [
+    eslint(),
+    dts({
+      include: [main],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
+    minify: false,
     outDir: 'lib',
+
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: main,
       name: 'providers',
       fileName: 'providers',
     },
