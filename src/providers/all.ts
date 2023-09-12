6fc1a74729a86248b1e1ea192e8dbe9a3dc11547
Isra <byzkk@protonmail.com>
2023-09-11 22:48:33 -0500

    feat(providers): support remotestream movies

--- src/providers/all.ts
+++ src/providers/all.ts
@@ -1,10 +1,11 @@
 import { Embed, Sourcerer } from '@/providers/base';
 import { upcloudScraper } from '@/providers/embeds/upcloud';
 import { flixhqScraper } from '@/providers/sources/flixhq/index';
+import { remotreamScraper } from '@/providers/sources/remotestream/index';
 
 export function gatherAllSources(): Array<Sourcerer> {
   // all sources are gathered here
-  return [flixhqScraper];
+  return [flixhqScraper, remotreamScraper];
 }
 
 export function gatherAllEmbeds(): Array<Embed> {
--- src/providers/sources/remotestream/index.ts
+++ src/providers/sources/remotestream/index.ts
@@ -5,22 +5,34 @@ import { NotFoundError } from '@/utils/errors';
 import { remotestreamBase } from './common';
 
 // TODO tv shows are available in flixHQ, just no scraper yet
-export const flixhqScraper = makeSourcerer({
+export const remotreamScraper = makeSourcerer({
   id: 'remotestream',
   name: 'Remote Stream',
   rank: 55,
   flags: [flags.NO_CORS],
   async scrapeShow(ctx) {
-    let playlistLink = `${remotestreamBase}/Movies/${ctx.media.tmdbId}`;
-
     const seasonNumber = ctx.media.season.number;
     const episodeNumber = ctx.media.episode.number;
 
-    playlistLink += `/${seasonNumber}/${episodeNumber}/${episodeNumber}.m3u8`;
+    const playlistLink = `${remotestreamBase}/Shows/${ctx.media.tmdbId}/${seasonNumber}/${episodeNumber}/${episodeNumber}.m3u8`;
 
     const streamRes = await ctx.fetcher<Blob>(playlistLink);
     if (streamRes.type !== 'application/x-mpegurl') throw new NotFoundError('No watchable item found');
-    ctx.progress(90);
+
+    return {
+      embeds: [],
+      stream: {
+        playlist: playlistLink,
+        type: 'hls',
+        flags: [flags.NO_CORS],
+      },
+    };
+  },
+  async scrapeMovie(ctx) {
+    const playlistLink = `${remotestreamBase}/Movies/${ctx.media.tmdbId}/${ctx.media.tmdbId}.m3u8`;
+
+    const streamRes = await ctx.fetcher<Blob>(playlistLink);
+    if (streamRes.type !== 'application/x-mpegurl') throw new NotFoundError('No watchable item found');
 
     return {
       embeds: [],
