9c6b61b4f970971ee7fe19df15e1056ce7d18caf
mrjvs <jellevs@gmail.com>
2023-09-14 21:07:35 +0200

    basic template for docs

--- .docs/app.config.ts
+++ .docs/app.config.ts
@@ -1,38 +1,17 @@
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
-    description: 'The best place to start your documentation.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    title: '@movie-web/providers',
+    description: 'For all your media scraping needs',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      github: 'movie-web/providers',
     },
-    github: {
-      dir: '.starters/default/content',
-      branch: 'main',
-      repo: 'docus',
-      owner: 'nuxt-themes',
-      edit: true
-    },
+    image: '',
     aside: {
       level: 0,
-      collapsed: false,
-      exclude: []
-    },
-    main: {
-      padded: true,
-      fluid: true
+      exclude: [],
     },
     header: {
-      logo: true,
-      showLinkIcon: true,
-      exclude: [],
-      fluid: true
-    }
-  }
-})
+      logo: false,
+    },
+  },
+});
