53b4098540445a7d3a0033e176534b099f9a6fb1
Isra <byzkk@protonmail.com>
2023-09-18 16:41:26 -0500

    Fix typo

--- src/providers/sources/kissasian/common.ts
+++ src/providers/sources/kissasian/common.ts
@@ -1,7 +1,7 @@
 import { mp4uploadScraper } from '@/providers/embeds/mp4upload';
 import { streamsbScraper } from '@/providers/embeds/streamsb';
 
-export const kissasianBase = 'https://kissasian.shp';
+export const kissasianBase = 'https://kissasian.sh';
 
 export const embedProviders = [
   {
