d95de0b60243ccaa601be3d686de41ba8cf07756
erynith <135774005+erynith@users.noreply.github.com>
2023-12-18 13:36:24 -0500

    Update search.ts

--- src/providers/sources/flixhq/search.ts
+++ src/providers/sources/flixhq/search.ts
@@ -33,10 +33,10 @@ export async function getFlixhqId(ctx: ScrapeContext, media: MovieMedia | ShowMe
     if (!v) return false;
 
     if (media.type === 'movie') {
-      return compareMedia(media, v.title, v.year)
-    }
+      return compareMedia(media, v.title, v.year);
+    };
 
-    return compareTitle(media.title, v.title) && media.season.number < v.seasons + 1
+    return compareTitle(media.title, v.title) && media.season.number < v.seasons + 1;
   });
 
   if (!matchingItem) return null;
