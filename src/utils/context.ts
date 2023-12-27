fb3f2378087f8e414f4458c8f10f1eac8f1d52f8
mrjvs <jellevs@gmail.com>
2023-12-27 22:33:13 +0100

    Better types for fetchers

--- src/utils/context.ts
+++ src/utils/context.ts
@@ -2,14 +2,8 @@ import { MovieMedia, ShowMedia } from '@/entrypoint/utils/media';
 import { UseableFetcher } from '@/fetchers/types';
 
 export type ScrapeContext = {
-  proxiedFetcher: {
-    <T>(...params: Parameters<UseableFetcher<T>>): ReturnType<UseableFetcher<T>>;
-    full<T>(...params: Parameters<UseableFetcher<T>['full']>): ReturnType<UseableFetcher<T>['full']>;
-  };
-  fetcher: {
-    <T>(...params: Parameters<UseableFetcher<T>>): ReturnType<UseableFetcher<T>>;
-    full<T>(...params: Parameters<UseableFetcher<T>['full']>): ReturnType<UseableFetcher<T>['full']>;
-  };
+  proxiedFetcher: UseableFetcher;
+  fetcher: UseableFetcher;
   progress(val: number): void;
 };
 
