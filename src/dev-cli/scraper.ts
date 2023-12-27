c9a611d6b765478ec81dadf956a452fb4aed7ca4
mrjvs <jellevs@gmail.com>
2023-12-27 22:27:37 +0100

    Add console logs to browser dev cli

--- src/dev-cli/scraper.ts
+++ src/dev-cli/scraper.ts
@@ -41,6 +41,7 @@ async function runBrowserScraping(
       args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
     const page = await browser.newPage();
+    page.on('console', (message) => console.log(`${message.type().slice(0, 3).toUpperCase()} ${message.text()}`));
     await page.goto(server.resolvedUrls.local[0]);
     await page.waitForFunction('!!window.scrape', { timeout: 5000 });
 
