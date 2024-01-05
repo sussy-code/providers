96ba9a85529f2c9cfcb6d6139c984ac6c3f9006d
dqvist12 <68880900+dqvist12@users.noreply.github.com>
2024-01-05 18:44:06 +0100

    Update index.ts
    + universalScraper

--- src/providers/sources/rezka/index.ts
+++ src/providers/sources/rezka/index.ts
@@ -1,6 +1,6 @@
 import { flags } from '@/entrypoint/utils/targets';
-import { makeSourcerer } from '@/providers/base';
-import { ScrapeContext } from '@/utils/context';
+import { SourcererOutput, makeSourcerer } from '@/providers/base';
+import { MovieScrapeContext, ScrapeContext, ShowScrapeContext } from '@/utils/context';
 import { NotFoundError } from '@/utils/errors';
 
 import { clearTrash, extractTitleAndYear, generateUUID, parseSubtitleLinks, parseVideoLinks } from './utils';
@@ -14,9 +14,23 @@ interface MovieData {
   type: 'show' | 'movie';
 }
 
+interface SeasonData {
+  number: number;
+  tmdbId: string;
+}
+
+interface RequestData {
+  id: string | null | undefined;
+  translator_id: number;
+  season: number | SeasonData;
+  episode: number | SeasonData;
+  favs: string;
+  action: string;
+}
+
 const rezkaBase = 'https://rezka.ag';
 
-async function getMovieReleaseDatesAndUrls({
+async function searchAndFindMedia({
   type,
   title,
   releaseYear,
@@ -58,12 +72,19 @@ async function getMovieReleaseDatesAndUrls({
   }
 }
 
-async function getStream(requestData: any, ctx: ScrapeContext): Promise<any> {
+async function getStream(requestData: RequestData, ctx: ScrapeContext): Promise<any> {
   try {
     const response = await ctx.proxiedFetcher<any>('/ajax/get_cdn_series/', {
       baseUrl: rezkaBase,
       method: 'POST',
-      body: new URLSearchParams(requestData).toString(),
+      body: new URLSearchParams({
+        id: requestData.id || '',
+        translator_id: requestData.translator_id.toString(),
+        season: requestData.season.toString(),
+        episode: requestData.episode.toString(),
+        favs: requestData.favs,
+        action: requestData.action,
+      }).toString(),
       headers: {
         // Content-type is needed to fetch
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
@@ -77,79 +98,45 @@ async function getStream(requestData: any, ctx: ScrapeContext): Promise<any> {
   }
 }
 
+const universalScraper = async (ctx: ShowScrapeContext | MovieScrapeContext): Promise<SourcererOutput> => {
+  const foundMedia = await searchAndFindMedia({
+    type: ctx.media.type === 'show' ? 'show' : 'movie',
+    title: ctx.media.title,
+    releaseYear: ctx.media.releaseYear,
+    ctx,
+  });
+
+  const requestData = {
+    id: foundMedia?.id,
+    translator_id: 238, // Standard value for original language
+    season: ctx.media.type === 'show' ? ctx.media.season : 1,
+    episode: ctx.media.type === 'show' ? ctx.media.episode : 1,
+    favs: generateUUID(), // Random UUID
+    action: ctx.media.type === 'show' ? 'get_episodes' : 'get_movie',
+  };
+  const { url: streamUrl, subtitle: streamSubtitle } = await getStream(requestData, ctx);
+  const parsedVideos = parseVideoLinks(clearTrash(streamUrl));
+  const parsedSubtitles = parseSubtitleLinks(streamSubtitle);
+
+  return {
+    embeds: [],
+    stream: [
+      {
+        id: 'primary',
+        type: 'file',
+        flags: [flags.CORS_ALLOWED],
+        captions: parsedSubtitles,
+        qualities: parsedVideos,
+      },
+    ],
+  };
+};
+
 export const rezkaScraper = makeSourcerer({
   id: 'rezka',
   name: 'Rezka HD',
-  rank: 90,
+  rank: 195,
   flags: [flags.CORS_ALLOWED],
-  async scrapeShow(ctx) {
-    const seasonNumber = ctx.media.season;
-    const episodeNumber = ctx.media.episode;
-    console.log('ctx', ctx);
-    const movieReturn = await getMovieReleaseDatesAndUrls({
-      type: 'show',
-      title: ctx.media.title,
-      releaseYear: ctx.media.releaseYear,
-      ctx,
-    });
-
-    const requestData = {
-      id: movieReturn?.id,
-      translator_id: 238,
-      season: seasonNumber,
-      episode: episodeNumber,
-      favs: generateUUID(),
-      action: 'get_episodes',
-    };
-
-    const { url: streamUrl, subtitle: streamSubtitle } = await getStream(requestData, ctx);
-    const parsedVideos = parseVideoLinks(clearTrash(streamUrl));
-    const parsedSubtitles = parseSubtitleLinks(streamSubtitle);
-
-    return {
-      embeds: [],
-      stream: [
-        {
-          id: 'primary',
-          type: 'file',
-          flags: [flags.CORS_ALLOWED],
-          captions: parsedSubtitles,
-          qualities: parsedVideos,
-        },
-      ],
-    };
-  },
-  async scrapeMovie(ctx) {
-    const movieReturn = await getMovieReleaseDatesAndUrls({
-      type: 'movie',
-      title: ctx.media.title,
-      releaseYear: ctx.media.releaseYear,
-      ctx,
-    });
-
-    const requestData = {
-      id: movieReturn?.id,
-      translator_id: 238,
-      season: 1,
-      episode: 1,
-      favs: generateUUID(),
-      action: 'get_movie',
-    };
-    const { url: streamUrl, subtitle: streamSubtitle } = await getStream(requestData, ctx);
-    const parsedVideos = parseVideoLinks(clearTrash(streamUrl));
-    const parsedSubtitles = parseSubtitleLinks(streamSubtitle);
-
-    return {
-      embeds: [],
-      stream: [
-        {
-          id: 'primary',
-          type: 'file',
-          flags: [flags.CORS_ALLOWED],
-          captions: parsedSubtitles,
-          qualities: parsedVideos,
-        },
-      ],
-    };
-  },
+  scrapeShow: universalScraper,
+  scrapeMovie: universalScraper,
 });
