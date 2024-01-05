fe1f8d364b3e162e73e41cdaaf451ee326f72e97
Jorrin <jorrinkievit@hotmail.com>
2024-01-05 14:51:54 +0100

    reintroduce the password endpoint

--- src/providers/embeds/vidsrc.ts
+++ src/providers/embeds/vidsrc.ts
@@ -2,6 +2,7 @@ import { flags } from '@/entrypoint/utils/targets';
 import { makeEmbed } from '@/providers/base';
 
 const hlsURLRegex = /file:"(.*?)"/;
+const setPassRegex = /var pass_path = "(.*set_pass\.php.*)";/;
 
 export const vidsrcembedScraper = makeEmbed({
   id: 'vidsrcembed', // VidSrc is both a source and an embed host
@@ -22,6 +23,22 @@ export const vidsrcembedScraper = makeEmbed({
     const finalUrl = atob(match);
 
     if (!finalUrl.includes('.m3u8')) throw new Error('Unable to find HLS playlist');
+
+    let setPassLink = html.match(setPassRegex)?.[1];
+    if (!setPassLink) throw new Error('Unable to find set_pass.php link');
+
+    if (setPassLink.startsWith('//')) {
+      setPassLink = `https:${setPassLink}`;
+    }
+
+    // VidSrc uses a password endpoint to temporarily whitelist the user's IP. This is called in an interval by the player.
+    // It currently has no effect on the player itself, the content plays fine without it.
+    // In the future we might have to introduce hooks for the frontend to call this endpoint.
+    await ctx.proxiedFetcher(setPassLink, {
+      headers: {
+        referer: ctx.url,
+      },
+    });
 
     return {
       stream: [
