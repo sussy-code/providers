7697c767214921084e1ba6c1af3fd528e75f19a3
Jorrin <jorrinkievit@hotmail.com>
2024-01-10 21:21:47 +0100

    add headers

--- src/providers/sources/remotestream.ts
+++ src/providers/sources/remotestream.ts
@@ -4,6 +4,9 @@ import { NotFoundError } from '@/utils/errors';
 
 const remotestreamBase = atob('aHR0cHM6Ly9mc2IuOG1ldDNkdGpmcmNxY2hjb25xcGtsd3hzeGIyb2N1bWMuc3RyZWFt');
 
+const origin = 'https://remotestre.am';
+const referer = 'https://remotestre.am/';
+
 export const remotestreamScraper = makeSourcerer({
   id: 'remotestream',
   name: 'Remote Stream',
@@ -19,6 +22,9 @@ export const remotestreamScraper = makeSourcerer({
     const streamRes = await ctx.fetcher.full(playlistLink, {
       method: 'HEAD',
       readHeaders: ['content-type'],
+      headers: {
+        Referer: referer,
+      },
     });
     if (!streamRes.headers.get('content-type')?.toLowerCase().includes('application/x-mpegurl'))
       throw new NotFoundError('No watchable item found');
@@ -33,6 +39,10 @@ export const remotestreamScraper = makeSourcerer({
           playlist: playlistLink,
           type: 'hls',
           flags: [flags.CORS_ALLOWED],
+          preferredHeaders: {
+            Referer: referer,
+            Origin: origin,
+          },
         },
       ],
     };
@@ -44,6 +54,9 @@ export const remotestreamScraper = makeSourcerer({
     const streamRes = await ctx.fetcher.full(playlistLink, {
       method: 'HEAD',
       readHeaders: ['content-type'],
+      headers: {
+        Referer: referer,
+      },
     });
     if (!streamRes.headers.get('content-type')?.toLowerCase().includes('application/x-mpegurl'))
       throw new NotFoundError('No watchable item found');
@@ -58,6 +71,10 @@ export const remotestreamScraper = makeSourcerer({
           playlist: playlistLink,
           type: 'hls',
           flags: [flags.CORS_ALLOWED],
+          preferredHeaders: {
+            Referer: referer,
+            Origin: origin,
+          },
         },
       ],
     };
