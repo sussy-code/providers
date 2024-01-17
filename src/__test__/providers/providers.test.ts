327ba025c2000625af7a474de682640473383dac
mrjvs <jellevs@gmail.com>
2024-01-17 18:18:08 +0100

    Enabled febbox subtitles

--- src/__test__/providers/providers.test.ts
+++ src/__test__/providers/providers.test.ts
@@ -1,8 +1,9 @@
-import { describe, expect, it } from "vitest";
 import { testSource } from "./providerUtils";
 import { lookmovieScraper } from "@/providers/sources/lookmovie";
 import { testMedia } from "./testMedia";
 import { showboxScraper } from "@/providers/sources/showbox";
+
+// TODO add embed testing
 
 testSource({
   source: lookmovieScraper,
