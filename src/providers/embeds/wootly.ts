8e4a87421c41ea34906dc6e7b7a4512c2d2b1d70
memecornucopia <memecornucopia@gmail.com>
2024-02-17 16:41:19 -0500

    Final Fixes

--- src/providers/embeds/wootly.ts
+++ src/providers/embeds/wootly.ts
@@ -17,13 +17,7 @@ export const wootlyScraper = makeEmbed({
     });
 
     const cookies = parseSetCookie(wootlyData.headers.get('Set-Cookie') || '');
-    let wootssesCookie = '';
-    let cookie = '';
-
-    if (cookies && cookies.wootsses) {
-      wootssesCookie = cookies.wootsses.value;
-      cookie = makeCookieHeader({ wootsses: wootssesCookie });
-    }
+    const wootssesCookie = cookies.wootsses.value;
 
     let $ = load(wootlyData.body); // load the html data
     const iframeSrc = $('iframe').attr('src') ?? '';
@@ -32,24 +26,18 @@ export const wootlyScraper = makeEmbed({
       method: 'GET',
       readHeaders: ['Set-Cookie'],
       headers: {
-        cookie,
+        cookie: makeCookieHeader({ wootsses: wootssesCookie }),
       },
     });
 
     const woozCookies = parseSetCookie(woozCookieRequest.headers.get('Set-Cookie') || '');
-    let woozCookie = '';
-    cookie = '';
-
-    if (cookies && woozCookies.wooz) {
-      woozCookie = woozCookies.wooz.value;
-      cookie = makeCookieHeader({ wooz: woozCookie });
-    }
+    const woozCookie = woozCookies.wooz.value;
 
     const iframeData = await ctx.proxiedFetcher<string>(iframeSrc, {
       method: 'POST',
       body: new URLSearchParams({ qdf: '1' }),
       headers: {
-        cookie,
+        cookie: makeCookieHeader({ wooz: woozCookie }),
         Referer: iframeSrc,
       },
     });
@@ -63,18 +51,13 @@ export const wootlyScraper = makeEmbed({
     const vd = scriptText.match(/vd=([^,]+)/)?.[0].replace(/vd=|["\s]/g, '');
 
     if (!tk || !vd) throw new Error('wootly source not found');
-    cookie = '';
-
-    if (woozCookie && wootssesCookie !== '') {
-      cookie = makeCookieHeader({ wooz: woozCookie, wootsses: wootssesCookie });
-    }
 
     const url = await ctx.proxiedFetcher<string>(`/grabd`, {
       baseUrl,
       query: { t: tk, id: vd },
       method: 'GET',
       headers: {
-        cookie,
+        cookie: makeCookieHeader({ wooz: woozCookie, wootsses: wootssesCookie }),
       },
     });
 
