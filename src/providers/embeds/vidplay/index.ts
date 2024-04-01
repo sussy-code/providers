38d4c97032a8ace0d479a65048beb6ee025f6147
TPN <tpn@ainta.dev>
2024-04-01 12:48:35 +0000

    use thumnails from vidplay

--- src/providers/embeds/vidplay/index.ts
+++ src/providers/embeds/vidplay/index.ts
@@ -3,7 +3,7 @@ import { makeEmbed } from '@/providers/base';
 import { Caption, getCaptionTypeFromUrl, labelToLanguageCode } from '@/providers/captions';
 
 import { getFileUrl } from './common';
-import { SubtitleResult, VidplaySourceResponse } from './types';
+import { SubtitleResult, ThumbnailTrack, VidplaySourceResponse } from './types';
 
 export const vidplayScraper = makeEmbed({
   id: 'vidplay',
@@ -18,6 +18,15 @@ export const vidplayScraper = makeEmbed({
     });
     if (typeof fileUrlRes.result === 'number') throw new Error('File not found');
     const source = fileUrlRes.result.sources[0].file;
+    const thumbnailSource = fileUrlRes.result.tracks.find((track) => track.kind === 'thumbnails');
+
+    let thumbnailTrack: ThumbnailTrack | undefined;
+    if (thumbnailSource) {
+      thumbnailTrack = {
+        type: 'vtt',
+        url: thumbnailSource.file,
+      };
+    }
 
     const url = new URL(ctx.url);
     const subtitlesLink = url.searchParams.get('sub.info');
@@ -47,6 +56,7 @@ export const vidplayScraper = makeEmbed({
           playlist: source,
           flags: [flags.CORS_ALLOWED],
           captions,
+          thumbnailTrack,
         },
       ],
     };
