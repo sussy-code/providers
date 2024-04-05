4238672f28bd7f8a5a5dc4e2fcd457d4fadc716e
TPN <tpn@ainta.dev>
2024-04-05 11:33:28 +0000

    fix smashystream

--- src/providers/all.ts
+++ src/providers/all.ts
@@ -1,9 +1,7 @@
 import { Embed, Sourcerer } from '@/providers/base';
 import { doodScraper } from '@/providers/embeds/dood';
-import { droploadScraper } from '@/providers/embeds/dropload';
 import { febboxHlsScraper } from '@/providers/embeds/febbox/hls';
 import { febboxMp4Scraper } from '@/providers/embeds/febbox/mp4';
-import { filelionsScraper } from '@/providers/embeds/filelions';
 import { mixdropScraper } from '@/providers/embeds/mixdrop';
 import { mp4uploadScraper } from '@/providers/embeds/mp4upload';
 import { streambucketScraper } from '@/providers/embeds/streambucket';
@@ -11,7 +9,6 @@ import { streamsbScraper } from '@/providers/embeds/streamsb';
 import { upcloudScraper } from '@/providers/embeds/upcloud';
 import { upstreamScraper } from '@/providers/embeds/upstream';
 import { vidsrcembedScraper } from '@/providers/embeds/vidsrc';
-import { vTubeScraper } from '@/providers/embeds/vtube';
 import { flixhqScraper } from '@/providers/sources/flixhq/index';
 import { goMoviesScraper } from '@/providers/sources/gomovies/index';
 import { kissAsianScraper } from '@/providers/sources/kissasian/index';
@@ -24,7 +21,7 @@ import { zoechipScraper } from '@/providers/sources/zoechip';
 import { closeLoadScraper } from './embeds/closeload';
 import { fileMoonScraper } from './embeds/filemoon';
 import { ridooScraper } from './embeds/ridoo';
-import { smashyStreamDScraper } from './embeds/smashystream/dued';
+import { smashyStreamOScraper } from './embeds/smashystream/opstream';
 import { smashyStreamFScraper } from './embeds/smashystream/video1';
 import { streamtapeScraper } from './embeds/streamtape';
 import { streamvidScraper } from './embeds/streamvid';
@@ -75,7 +72,7 @@ export function gatherAllEmbeds(): Array<Embed> {
     vidsrcembedScraper,
     streambucketScraper,
     smashyStreamFScraper,
-    smashyStreamDScraper,
+    smashyStreamOScraper,
     ridooScraper,
     closeLoadScraper,
     fileMoonScraper,
@@ -85,8 +82,5 @@ export function gatherAllEmbeds(): Array<Embed> {
     streamvidScraper,
     voeScraper,
     streamtapeScraper,
-    droploadScraper,
-    filelionsScraper,
-    vTubeScraper,
   ];
 }
