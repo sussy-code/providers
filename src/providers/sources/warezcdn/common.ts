0a2259b1f4a8d6c67b6e031256b9433b966b41c9
lonelil <51315646+lonelil@users.noreply.github.com>
2024-04-12 16:59:51 +0800

    remove user-agent

--- src/providers/sources/warezcdn/common.ts
+++ src/providers/sources/warezcdn/common.ts
@@ -14,8 +14,6 @@ export async function getExternalPlayerUrl(ctx: ScrapeContext, embedId: string, 
     baseUrl: warezcdnApiBase,
     headers: {
       Referer: `${warezcdnApiBase}/getEmbed.php?${new URLSearchParams(params)}`,
-      'User-Agent':
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
     },
     query: params,
   });
