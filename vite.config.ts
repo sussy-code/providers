7f79620b79aeb39cca4c5bb7c9de97b43ea64a5b
XSWY <134489068+0pt1onz@users.noreply.github.com>
2026-03-14 19:46:38 +0000

    New source

--- vite.config.ts
+++ vite.config.ts
@@ -11,41 +11,40 @@ if (shouldTestProviders) tests = ['src/__test__/providers/**/*.test.ts'];
 export default defineConfig((env) => ({
   plugins: [
     env.mode !== 'test' && eslintPlugin(),
-    dts({
-      rollupTypes: true,
-    }),
+    dts({ rollupTypes: true }),
   ],
-  css: {
-    postcss: {
-      plugins: [],
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  rollupOptions: {
-  external: [
-    ...Object.keys(pkg.dependencies),
-    'puppeteer',
-    '@puppeteer/browsers',
-    'proxy-agent',
-    'node:http',
-    'node:https',
-    'node:url',
-  ],
-  output: {
-    globals: Object.fromEntries(
-      Object.keys(pkg.dependencies).map((v) => [v, v])
-    ),
-  },
-    outDir: 'lib',
+  build: {
+    minify: false,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'index',
       fileName: 'index',
       formats: ['umd', 'es'],
+    },
+    outDir: 'lib',
+    emptyOutDir: true,
+    target: 'esnext',
+    rollupOptions: {
+      // Externalize dependencies that should not be bundled
+      external: [
+        ...Object.keys(pkg.dependencies),
+        'puppeteer',
+        '@puppeteer/browsers',
+        'proxy-agent',
+        'node:http',
+        'node:https',
+        'node:url',
+      ],
+      output: {
+        globals: Object.fromEntries(
+          Object.keys(pkg.dependencies).map((v) => [v, v])
+        ),
+      },
     },
   },
   test: {
